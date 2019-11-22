c9ee0fa9dd8767b78a5c94911e73bef68605a73d
Renato Groffe <renato.groffe@yahoo.com.br>
2019-11-22 01:27:51 -0300

    ASP.NET Core 3.0 + MVC + FluentValidation + Globalization
    ASP.NET Core 3.0 + MVC + FluentValidation + Globalization

---
+++ ConversorDistancias/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ConversorDistancias.Models;
+
+namespace ConversorDistancias.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DistanciasViewModel distancias)
+        {
+            if (ModelState.IsValid)
+            {
+                distancias.DistanciaKm = Convert.ToDouble(distancias.DistanciaMilhas) * 1.609;
+                return View(distancias);
+            }
+            else
+            {
+                ModelState.Clear();
+                ModelState.AddModelError(nameof(distancias.DistanciaMilhas),
+                    "Informe uma distância em milhas entre 0,01 e 9.999.999,99!");
+                return View("Index", distancias);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConversorDistancias/Models/DistanciasViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConversorDistancias.Models
+{
+    public class DistanciasViewModel
+    {
+        public double? DistanciaMilhas { get; set; }
+
+        public double? DistanciaKm { get; set; }
+    }
+}
